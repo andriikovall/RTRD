03c63297cad6f785034dd33900cf94b35b98061f
Yaroslaw Klymenko <yaroslaw.klymko@gmail.com>
2020-02-23 06:11:52 +0200

    event creation

--- server/src/models/Event.js
+++ server/src/models/Event.js
@@ -29,7 +29,10 @@ const eventSchema = new Schema({
             default: 0,
         },
     }],
-    region: String,
+    region: {
+        type: String,
+        default: "Оболонь",
+    },
     date: String,
 });
 
